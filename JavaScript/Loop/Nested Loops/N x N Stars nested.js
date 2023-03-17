5b176d84d942c9e32b602ac7c61abb7ca81f90a0
WebCoders1122 <webcoders1122@gmail.com>
2023-03-17 18:17:15 +0500

    N x N letters
    using nested loops of JS

---
+++ JavaScript/Loop/Nested Loops/N x N Stars nested.js
@@ -0,0 +1,8 @@
+// Made of N x N Stars using nested loop
+let x = 6;
+for(a = 1; a <= x; a++){
+    for(b = 1; b < x; b++){
+        document.write('*');
+    };
+    document.write("*" + '<br>');
+};
