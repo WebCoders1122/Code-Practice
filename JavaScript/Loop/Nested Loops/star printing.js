641e7ceb299f135ecc44ed1b0b8284b0d0747737
WebCoders1122 <webcoders1122@gmail.com>
2023-03-17 18:17:48 +0500

    Star tree
    nested loop is used

---
+++ JavaScript/Loop/Nested Loops/star printing.js
@@ -0,0 +1,7 @@
+// writing program using nested loop for increasing star number in line
+for(a = 1; a < 6; a++){
+    for(b = 1; b < a + 1; b++){
+        document.write('*')
+    };
+    document.write('<br>');
+};
