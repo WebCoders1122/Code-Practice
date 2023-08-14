bfaf7a83381b57505e6ab9113302622e0087891c
WebCoders1122 <webcoders1122@gmail.com>
2023-08-14 07:30:32 +0500

    Update in workout app

--- JavaScript/JavaScript Projects/Workout Application/JS/tracker.js
+++ JavaScript/JavaScript Projects/Workout Application/JS/tracker.js
@@ -1,24 +1,26 @@
 export class workout {
-    constructor(container) {
-        this.container = container;
-        this.container.innerHTML = workout.table().trim();
-        this.localArray = JSON.parse(localStorage.getItem(workout.Local_array_key) || "[]");
-        this.showUpdate()
-        this.container.querySelector('.add-btn').addEventListener('click', () => {
-            let newDate = new Date(),
-                day = newDate.getDate().toString().padStart(2, '0'),
-                month = (newDate.getMonth() + 1).toString().padStart(2, '0'),
-                year = newDate.getFullYear();
-            this.addNewEntry({
-                date: `${year}-${month}-${day}`,
-                work: 'Walking',
-                duration: 15
-            })
-        })
-    }
-    static Local_array_key = 'nhsdfkkobkjdfoe';
-    static table() {
-        return `<table>
+  constructor(container) {
+    this.container = container;
+    this.container.innerHTML = workout.table().trim();
+    this.localArray = JSON.parse(
+      localStorage.getItem(workout.Local_array_key) || "[]"
+    );
+    this.showUpdate();
+    this.container.querySelector(".add-btn").addEventListener("click", () => {
+      let newDate = new Date(),
+        day = newDate.getDate().toString().padStart(2, "0"),
+        month = (newDate.getMonth() + 1).toString().padStart(2, "0"),
+        year = newDate.getFullYear();
+      this.addNewEntry({
+        date: `${year}-${month}-${day}`,
+        work: "Walking",
+        duration: 15,
+      });
+    });
+  }
+  static Local_array_key = "nhsdfkkobkjdfoe";
+  static table() {
+    return `<table>
                     <thead class="header">
                         <tr>
                             <th>Date</th>
@@ -37,10 +39,10 @@ export class workout {
                             </td>
                         </tr>
                     </tbody>
-                </table>`
-    }
-    static entries() {
-        return `<tr>
+                </table>`;
+  }
+  static entries() {
+    return `<tr>
                     <td>
                         <input type="date" class="dates">
                     </td>
@@ -62,59 +64,66 @@ export class workout {
                     <td>
                         <button class="close">&times;</button>
                     </td>
-                </tr>`
-    }
-    showUpdate() {
-        const entries = this.container.querySelector('.entries');
-        entries.innerHTML = ``;
-        const addData = (element) => {
-            const template = document.createElement('template');
-            template.innerHTML = workout.entries();
-            let row = template.content.firstElementChild;
-            row.querySelector('.dates').value = element.date;
-            row.querySelector('#workout-options').value = element.work;
-            row.querySelector('.duration').value = element.duration;
+                </tr>`;
+  }
+  showUpdate() {
+    const entries = this.container.querySelector(".entries");
+    entries.innerHTML = ``;
+    const addData = element => {
+      const template = document.createElement("template");
+      template.innerHTML = workout.entries();
+      let row = template.content.firstElementChild;
+      row.querySelector(".dates").value = element.date;
+      row.querySelector("#workout-options").value = element.work;
+      row.querySelector(".duration").value = element.duration;
 
-            //to update
-            row.querySelector('.dates').addEventListener('change', ({ target }) => {
-                element.date = target.value;
-                this.saveToLocal();
-            })
-            row.querySelector('#workout-options').addEventListener('change', ({ target }) => {
-                element.work = target.value;
-                this.saveToLocal();
-            })
-            row.querySelector('.duration').addEventListener('change', ({ target }) => {
-                element.duration = target.value;
-                this.saveToLocal();
-            })
-            row.querySelector('.close').addEventListener('click',() => {
-                this.deleteEntry(element);
-            })
-            entries.appendChild(row)
-        };
-        this.localArray.forEach(element => {
-            addData(element)
-        });
-    }
-    deleteEntry(delElement){
-        // this.localArray.forEach((element, index) => {
-        //     if (element !== delElement) {
-        //         this.localArray.splice(index, 1)
-        //     }
-        // })
-        this.localArray = this.localArray.filter((element) => {
-            return element !== delElement
-        })
-        this.saveToLocal()
-        this.showUpdate()
-    }
-    saveToLocal() {
-        localStorage.setItem(workout.Local_array_key, JSON.stringify(this.localArray))
-    }
-    addNewEntry(data) {
-        this.localArray.push(data);
+      //to update
+      row.querySelector(".dates").addEventListener("change", ({ target }) => {
+        element.date = target.value;
         this.saveToLocal();
-        this.showUpdate();
-    }
+      });
+      row
+        .querySelector("#workout-options")
+        .addEventListener("change", ({ target }) => {
+          element.work = target.value;
+          this.saveToLocal();
+        });
+      row
+        .querySelector(".duration")
+        .addEventListener("change", ({ target }) => {
+          element.duration = target.value;
+          this.saveToLocal();
+        });
+      row.querySelector(".close").addEventListener("click", () => {
+        this.deleteEntry(element);
+      });
+      entries.appendChild(row);
+    };
+    this.localArray.forEach(element => {
+      addData(element);
+    });
+  }
+  deleteEntry(delElement) {
+    // this.localArray.forEach((element, index) => {
+    //     if (element !== delElement) {
+    //         this.localArray.splice(index, 1)
+    //     }
+    // })
+    this.localArray = this.localArray.filter(element => {
+      return element !== delElement;
+    });
+    this.saveToLocal();
+    this.showUpdate();
+  }
+  saveToLocal() {
+    localStorage.setItem(
+      workout.Local_array_key,
+      JSON.stringify(this.localArray)
+    );
+  }
+  addNewEntry(data) {
+    this.localArray.push(data);
+    this.saveToLocal();
+    this.showUpdate();
+  }
 }
