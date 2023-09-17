d3c7e2514b6db7e0fdfe285a32479910c9651f97
WebCoders1122 <webcoders1122@gmail.com>
2023-09-17 18:15:27 +0500

    Final Update with all fixes
    code optimized
    animations added
    chat open and close button working now
    chat messuages width fixed
    error message background color

--- JavaScript/JavaScript Projects/Chatbot/app.js
+++ JavaScript/JavaScript Projects/Chatbot/app.js
@@ -3,8 +3,12 @@ const send_button = document.querySelector('.chat_input i');
 const input_area = document.querySelector('.chat_input textarea');
 const chat_body = document.querySelector('.chat_body');
 let input_message;
+const chat_toggle_close_btn = document.querySelector('.fa-xmark');
+const chat_toggle_open_btn = document.querySelector('.fa-comment-medical');
+const chat_close_btn = document.querySelector('.chat_close'); // for mobiles etc
+let input_area_initial_height = input_area.scrollHeight;
 // open AI data
-const ai_api_key = `Your API key`;
+const ai_api_key = `Your API Key`;
 const ai_url = `https://api.openai.com/v1/chat/completions`;
 
 // Misc functions
@@ -32,6 +36,7 @@ const chat_handler = () => {
         gen_response(reply)
     }, 400)
     chat_body.scrollTo(0, chat_body.scrollHeight);
+    input_area.style.height = `${input_area_initial_height}px`
 };
 
 // 01 to get input and insert to chat_body
@@ -43,6 +48,7 @@ document.addEventListener('keydown', (key) => {
         key.preventDefault();
         chat_handler();
         chat_body.scrollTo(0, chat_body.scrollHeight);
+        input_area.style.height = `${input_area_initial_height}px`
     }
 })
 //03 to get response from AI website
@@ -64,84 +70,30 @@ const gen_response = (reply) => {
         })
     }
     fetch(ai_url, res).then(response => response.json()).then(data => {
-        let response_message = reply.querySelector('p');
-        response_message.innerText = data.choices[0].message.content;
+        reply.innerHTML = `<i class="fa-solid fa-robot"></i><p>${data.choices[0].message.content}</p>`
         chat_body.scrollTo(0, chat_body.scrollHeight);
         console.log(data)
     }).catch((error) => {
-        console.log(error)
+        let response_message = reply.querySelector('p');
+        response_message.innerText = 'Something Went Wrong... Try Again.'
+        response_message.classList.add('error')
     })
 };
 
+//04 show hide chatbot
+chat_toggle_open_btn.addEventListener('click', () => {
+    document.querySelector('body').classList.add('chat_active')
+})
+chat_toggle_close_btn.addEventListener('click', () => {
+    document.querySelector('body').classList.remove('chat_active')
+})
+chat_close_btn.addEventListener('click', () => {
+    document.querySelector('body').classList.remove('chat_active')
+})
 
+//05 to increase height of textarea while typing multiple lines
+input_area.addEventListener('input', (event) => {
+    input_area.style.height = `${input_area_initial_height}px`
+    input_area.style.height = `${input_area.scrollHeight}px`
+})
 
-
-
-// //Misc Variables
-// const textarea = document.querySelector('.chat-input textarea');
-// const send_button = document.querySelector('#send-btn');
-// const chat_area = document.querySelector('.chatbox');
-// let message;
-
-// // AI veriables
-// const AI_Chat_URL = `https://api.openai.com/v1/chat/completions`;
-// const AI_Chat_API = `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`;
-
-
-// //Misc Functions
-
-// //03
-// const generate_response = (incoming_li) => {
-//     let request = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${AI_Chat_API}`
-//         },
-//         body: JSON.stringify({
-//             "model": "gpt-3.5-turbo",
-//             "messages": [{
-//                 "role": "user",
-//                 "content": message
-//             }]
-//         })
-//     }
-//     let p = incoming_li.querySelector('p');
-//     fetch(AI_Chat_URL, request).then(res => res.json()).then(data => {
-//         p.innerText = data.choices[0].message.content;
-//     }).catch(error => {
-//         p.innerText = 'OOPs... Somthing Went Wrong, Please Try Again..';
-//     }).finally(() => {
-//         chat_area.scrollTo(0, chat_area.scrollHeight)
-//     })
-// };
-
-// //02
-// const insert_message_in_chatbox = (message, html_class) => {
-//     let li = document.createElement('li');
-//     li.classList.add('chat', html_class);
-//     let chat_content = html_class === 'outgoing' ? `<p>${message}</p>` : `
-//                 <span class="material-symbols-outlined">chat</span>
-//                 <p>${message}</p>`;
-//     li.innerHTML = chat_content;
-//     chat_area.scrollTo(0, chat_area.scrollHeight)
-//     return li;
-// };
-
-// //01
-// const message_handler = () => {
-//     message = textarea.value;
-//     chat_area.appendChild(insert_message_in_chatbox(message, 'outgoing'))
-//     chat_area.scrollTo(0, chat_area.scrollHeight)
-//     textarea.value = ``;
-//     setTimeout(() => {
-//         let incoming_li = insert_message_in_chatbox('Thinking...', 'incoming');
-//         chat_area.appendChild(incoming_li)
-//         generate_response(incoming_li);
-//     }, 600)
-
-// };
-
-
-// //01 get textarea message and place in the chatbox
-// send_button.addEventListener('click', message_handler)
