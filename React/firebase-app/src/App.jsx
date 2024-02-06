62b802dc7113b90a6990595858e27b292c718c10
WebCoders1122 <121373954+WebCoders1122@users.noreply.github.com>
2024-02-06 12:51:16 +0500

    Firebase authentication complete
    lacture 4 completed

--- React/firebase-app/src/App.jsx
+++ React/firebase-app/src/App.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { app } from "./firebase";
-import { getDatabase, ref, set } from "firebase/database";
-
-const db = getDatabase(app);
+import SignUpPage from "./Pages/SignUpPage";
 
 function App() {
-  const putData = () => {
-    set(ref(db, "user/shakir"), {
-      id: 1,
-      name: "shakir sb",
-      age: 24,
-    });
-  };
   return (
     <div>
       <h1>Firebase App</h1>
-      <button onClick={putData}>Put Data</button>{" "}
+      <SignUpPage />
     </div>
   );
 }
