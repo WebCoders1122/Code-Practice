4355d9dfba1de1085ca13458c60d5f9a99de2aa7
WebCoders1122 <121373954+WebCoders1122@users.noreply.github.com>
2024-02-12 02:06:32 +0500

    firebase sign in completed

--- React/firebase-app/src/App.jsx
+++ React/firebase-app/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import SignUpPage from "./Pages/SignUpPage";
+import SigninPage from "./Pages/SigninPage";
 
 function App() {
   return (
     <div>
       <h1>Firebase App</h1>
       <SignUpPage />
+      <SigninPage />
     </div>
   );
 }
--- React/firebase-app/src/Pages/SigninPage.jsx
+++ React/firebase-app/src/Pages/SigninPage.jsx
@@ -1,57 +1,56 @@
 import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth(app);
+let auth = getAuth(app);
 
-const SignUpPage = () => {
+const SigninPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const createUser = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((value) => console.log("creation user success"))
+  const signInUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => console.log("Sign in Success"))
       .catch((err) => console.log(err));
   };
   return (
-    <div className='signup_page'>
-      <h1 className='heading text-3xl font-bold m-4 mt-10'>Sign Up Form</h1>
-
+    <div className='signin_page'>
+      <h1 className='heading text-3xl font-bold m-4 mt-10'>Sign in Form</h1>
       <label
-        htmlFor='email'
+        htmlFor='sign-in-email'
         className=' text-xl font-bold drop-shadow-lg m-2'>
         Email:
       </label>
       <input
         className='bg-slate-50 border py-2 px-4 m-1 rounded-md text-gray-600 font-semibold'
         type='email'
-        id='email'
+        id='sign-in-email'
         placeholder='Your Email...'
         onChange={(e) => setEmail(e.target.value)}
         value={email}
       />
       <br />
       <label
-        htmlFor='password'
+        htmlFor='sign-in-password'
         className=' text-xl font-bold drop-shadow-lg m-2'>
         Password:
       </label>
       <input
         className='bg-slate-50 border py-2 px-4 m-1 rounded-md text-gray-600 font-semibold'
         type='password'
-        id='password'
+        id='sign-in-password'
         placeholder='Your Password...'
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
       <br />
       <button
-        onClick={createUser}
+        onClick={signInUser}
         className='bg-slate-600 text-white text-2xl font-bold py-2 px-5 m-3 rounded-lg'>
-        Sign Up
+        Sign In
       </button>
     </div>
   );
 };
 
-export default SignUpPage;
+export default SigninPage;
