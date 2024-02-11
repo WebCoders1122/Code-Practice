46e826cb2603ba41cc119905f4e612f3344c4d53
WebCoders1122 <121373954+WebCoders1122@users.noreply.github.com>
2024-02-11 11:03:41 +0500

    star rating included
    Star rating

--- React/react-practice-projects/src/App.jsx
+++ React/react-practice-projects/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Accordian from "./components/Accordion/Accordian";
 import RandomColor from "./components/Random Color Generator/RandomColor";
+import StarRating from "./components/Star_Rating/StarRating";
 const App = () => {
   return (
     <div className=''>
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </div>
   );
 };
